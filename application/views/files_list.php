88488643823e5312cd2b0c483070e4cf3bba51ff
Rafael Souza Borges <Rafael Souza Borges@172.20.18.153>
2017-09-27 11:56:54 -0300

    atualizando forma de enviar o formulário

--- application/views/files_list.php
+++ application/views/files_list.php
@@ -14,14 +14,15 @@
                     <img class="rounded-circle" src="<?php echo base_url('images/uploads/'); echo $file; ?>" width="140" height="140">
                     <p>
                         <?php
-                            echo form_open('files/do_rename');
+                            echo form_open('files/do_rename', 'id="formulario"');
                             echo form_input(array('name' => 'name', 'class' => 'form-control-plaintext', 'value' => $no_extension));
                             echo form_input(array('type' => 'hidden', 'name' => 'file', 'value' => $file));
-                            echo form_button(array('type' => 'submit', 'class' => 'btn btn-warning', 'content' => '<i class="fa fa-pencil fa-fw"></i> Renomear'));
+                            //echo form_button(array('type' => 'submit', 'class' => 'btn btn-warning', 'content' => '<i class="fa fa-pencil fa-fw"></i> Renomear'));
                             echo form_close();
                         ?>
                     </p>
                     <p>
+                        <a class="btn btn-warning" onclick="submitForm()"><i class="fa fa-pencil fa-fw"></i> Renomear</a>
                         <a class="btn btn-warning" href="do_trash/<?php echo $file; ?>"><i class="fa fa-trash-o fa-fw"></i> Lixeira</a>
                     </p>
                     <!--<p>
@@ -45,3 +46,8 @@
                 </div><!-- /.col-lg-4 -->
             <?php endforeach; ?>
             </div><!-- /.row -->
+            <script>
+                function submitForm() {
+                    document.getElementById("formulario").submit();
+                }
+            </script>

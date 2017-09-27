20f40cd7fc35a7c4f58741079ef7594a1a318bbe
Rafael Souza Borges <Rafael Souza Borges@172.20.18.153>
2017-09-27 12:21:09 -0300

    atualizando buttons

--- application/views/files_list.php
+++ application/views/files_list.php
@@ -24,8 +24,8 @@
                 </p>
                 <p>
                 <div class="btn-group" role="group" aria-label="Basic example">
-                    <a class="btn btn-warning" onclick="submitForm()"><i class="fa fa-pencil fa-fw"></i> Renomear</a>
-                    <a class="btn btn-warning" href="do_trash/<?php echo $file; ?>"><i class="fa fa-trash-o fa-fw"></i> Lixeira</a>
+                    <button class="btn btn-warning" onclick="submitForm()"><i class="fa fa-pencil fa-fw"></i> Renomear</button>
+                    <button class="btn btn-warning" href="do_trash/<?php echo $file; ?>"><i class="fa fa-trash-o fa-fw"></i> Lixeira</button>
                 </div>
                 </p>                    
             </div><!-- /.col-lg-4 -->
